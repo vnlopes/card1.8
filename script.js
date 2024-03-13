93a4c2916fa3e9acfd8b33d1011368f816a03c17
vnlopes <viniciuslopesdasilva01@gmail.com>
2024-03-12 21:43:26 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const anima = () => {
+    document.querySelector('#card-top').classList.add('card-top')
+    document.querySelector('#c1').classList.add('c1')
+    document.querySelector('#c2').classList.add('c2')
+    document.querySelector('#c3').classList.add('c3')
+    document.querySelector('#cartao').classList.add('cartao')
+    document.querySelector('#light').classList.add('light')
+}
+
+const remove = () => {
+    document.querySelector('#card-top').classList.remove('card-top')
+    document.querySelector('#c1').classList.remove('c1')
+    document.querySelector('#c2').classList.remove('c2')
+    document.querySelector('#c3').classList.remove('c3')
+    document.querySelector('#cartao').classList.remove('cartao')
+    document.querySelector('#light').classList.remove('light')
+}
